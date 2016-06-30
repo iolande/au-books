3744cb092e417416220467b8876a0670bcb3df10
io <iolande@users.noreply.github.com>
2016-06-30 23:25:35 +0100

    Add default route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  configureRouter(config, router) {
+    config.title = 'Books';
+    config.map([
+      { route: '', name: 'home', moduleId: 'routes/index', nav: true, title: 'Home' }
+    ]);
+    this.router = router;
   }
 }
--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -1,7 +1,38 @@
 import {App} from '../../src/app';
 
-describe('the app', () => {
-  it('says hello', () => {
-    expect(new App().message).toBe('Hello World!');
+class RouterStub {
+  configure(handler) {
+    handler(this);
+  }
+
+  map(routes) {
+    this.routes = routes;
+  }
+}
+
+describe('the App module', () => {
+  let sut;
+  let mockedRouter;
+
+  beforeEach(() => {
+    mockedRouter = new RouterStub();
+    sut = new App();
+    sut.configureRouter(mockedRouter, mockedRouter);
+  });
+
+  afterEach(() => {
+    sut = null;
+  });
+
+  it('contains a router property', () => {
+    expect(sut.router).toBeDefined();
+  });
+
+  it('configures the router title', () => {
+    expect(sut.router.title).toEqual('Books');
+  });
+
+  it('should have a default route', () => {
+    expect(sut.router.routes).toContain({ route: '', name: 'home', moduleId: 'routes/index', nav: true, title: 'Home' });
   });
 });
