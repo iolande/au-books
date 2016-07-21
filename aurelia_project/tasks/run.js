21b39ba87b7b2e1f96321cf4bf6f94f62a4558f4
io <iolande@users.noreply.github.com>
2016-07-21 21:16:31 +0100

    Update aurelia files from new CLI build

--- aurelia_project/tasks/run.js
+++ aurelia_project/tasks/run.js
@@ -1,5 +1,6 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
+import historyApiFallback from 'connect-history-api-fallback/lib';
 import project from '../aurelia.json';
 import build from './build';
 import {CLIOptions} from 'aurelia-cli';
@@ -18,16 +19,22 @@ let serve = gulp.series(
   done => {
     browserSync({
       online: false,
-      open: false,
+      // open: false,
       port: 9000,
+      logLevel: 'silent',
       server: {
         baseDir: ['.'],
-        middleware: function(req, res, next) {
+        middleware: [historyApiFallback(), function(req, res, next) {
           res.setHeader('Access-Control-Allow-Origin', '*');
           next();
-        }
+        }]
       }
-    }, done);
+    }, (err, bs) => {
+      let urls = bs.options.get('urls').toJS();
+      console.log(`Application Available At: ${urls.local}`);
+      console.log(`BrowserSync Available At: ${urls.ui}`);
+      done();
+    });
   }
 );
 
@@ -39,8 +46,8 @@ let refresh = gulp.series(
 let watch = function() {
   gulp.watch(project.transpiler.source, refresh).on('change', onChange);
   gulp.watch(project.markupProcessor.source, refresh).on('change', onChange);
-  gulp.watch(project.cssProcessor.source, refresh).on('change', onChange)
-}
+  gulp.watch(project.cssProcessor.source, refresh).on('change', onChange);
+};
 
 let run;
 
