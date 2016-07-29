fbe031f9a812e47ad1602029118893b22de4fb30
io <iolande@users.noreply.github.com>
2016-07-29 20:36:23 +0100

    Remove datacontext from main.js setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import environment from './environment';
 import {BookService} from './services/book-service';
-import {Datacontext} from './services/datacontext';
 
 Promise.config({
   warnings: {
@@ -11,7 +10,6 @@ Promise.config({
 export function configure(aurelia) {
   aurelia.use
     .singleton('BookService', BookService)
-    .singleton('Datacontext', Datacontext)
     .standardConfiguration()
     .feature('resources');
 
