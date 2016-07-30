28c513f5c77dca9cd3227af5f77073fd7f128e08
io <iolande@users.noreply.github.com>
2016-07-30 21:39:17 +0100

    Wire up the proper case converter & tweak

--- src/resources/value-converters/proper-case.js
+++ src/resources/value-converters/proper-case.js
@@ -4,20 +4,26 @@ export class ProperCaseValueConverter {
 
     const words = value.split(' ');
     let processedWords = words.map(word => {
-      return word[0].toUpperCase() + word.slice(1);
+      return word[0] ? word[0].toUpperCase() + word.slice(1) : word;
     })
     .map(word => {
+      if (!word[0]) { return word; }
+      // TODO: speed this up with regex?
       let returnWord = word;
 
       const firstTwoLetters = word.substr(0, 2).toLowerCase();
       if (firstTwoLetters === 'mc' || firstTwoLetters === 'o\'') {
-        returnWord = word[0].toUpperCase() + word.substr(1, 1) + word[2].toUpperCase() + word.slice(3);
+        returnWord = word[0] ? word[0].toUpperCase() + word.substr(1, 1) : '';
+        returnWord += word[2] ? word[2].toUpperCase() + word.slice(3) : '';
+
+        return returnWord;
       }
 
       const firstThreeLetters = word.substr(0, 3).toLowerCase();
       const vowels = ['a', 'e', 'i', 'o', 'u'];
-      if (firstThreeLetters === 'mac' && vowels.indexOf(word[4]) > -1) {
-        returnWord = word[0].toUpperCase() + word.substr(1, 2) + word[3].toUpperCase() + word.slice(4);
+      if (firstThreeLetters === 'mac' && word[4] && vowels.indexOf(word[4].toLowerCase()) > -1) {
+        returnWord = word[0] ? word[0].toUpperCase() + word.substr(1, 2) : '';
+        returnWord += word[3] ? word[3].toUpperCase() + word.slice(4) : '';
       }
 
       return returnWord;
