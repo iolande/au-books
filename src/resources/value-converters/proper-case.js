c484c07d14304bc614d80d226689e43f7ffae03b
io <iolande@users.noreply.github.com>
2016-07-30 21:04:39 +0100

    Apply capitalisation rules per spec

--- src/resources/value-converters/proper-case.js
+++ src/resources/value-converters/proper-case.js
@@ -1,5 +1,28 @@
 export class ProperCaseValueConverter {
   toView(value) {
-    return value;
+    if (!value) { return ''; }
+
+    const words = value.split(' ');
+    let processedWords = words.map(word => {
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    .map(word => {
+      let returnWord = word;
+
+      const firstTwoLetters = word.substr(0, 2).toLowerCase();
+      if (firstTwoLetters === 'mc' || firstTwoLetters === 'o\'') {
+        returnWord = word[0].toUpperCase() + word.substr(1, 1) + word[2].toUpperCase() + word.slice(3);
+      }
+
+      const firstThreeLetters = word.substr(0, 3).toLowerCase();
+      const vowels = ['a', 'e', 'i', 'o', 'u'];
+      if (firstThreeLetters === 'mac' && vowels.indexOf(word[4]) > -1) {
+        returnWord = word[0].toUpperCase() + word.substr(1, 2) + word[3].toUpperCase() + word.slice(4);
+      }
+
+      return returnWord;
+    });
+
+    return processedWords.join(' ');
   }
 }
--- test/unit/resources/value-converters/proper-case.spec.js
+++ test/unit/resources/value-converters/proper-case.spec.js
@@ -18,9 +18,42 @@ describe('ProperCaseValueConverter', () => {
       expect(result).toBe('A');
     });
 
-    it('third letter if the first two were O\'');
-    it('third letter if the first two were Mc');
-    it('fourth letter if the first three were Mac and the fourth is not a vowel');
-    it('first letter after a space');
+    it('third letter if the first two were O\'', () => {
+      const result = sut.toView('o\'brian');
+
+      expect(result).toBe('O\'Brian');
+    });
+
+    it('third letter if the first two were Mc', () => {
+      const result = sut.toView('mcdonald');
+
+      expect(result).toBe('McDonald');
+    });
+
+    it('fourth letter if the first three were Mac and the fourth is not a vowel', () => {
+      const result = sut.toView('macmillan');
+
+      expect(result).toBe('MacMillan');
+    });
+
+    it('first letter after a space (not mc, o\', or mac)', () => {
+      const result = sut.toView('a name with spaces');
+
+      expect(result).toBe('A Name With Spaces');
+    });
+
+    it('first letter after a space (with mc, o\', and mac)', () => {
+      const result = sut.toView('a mcarthur, macnair and o\'donnell name');
+
+      expect(result).toBe('A McArthur, MacNair And O\'Donnell Name');
+    });
+  });
+
+  describe('toView should not capitalise the', () => {
+    it('fourth letter if the first three were Mac and the fourth is a vowel', () => {
+      const result = sut.toView('macalpine');
+
+      expect(result).toBe('Macalpine');
+    });
   });
 });
