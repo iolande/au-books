eaa027990045f91f044d458b6903055160620afc
io <iolande@users.noreply.github.com>
2016-08-12 20:31:19 +0100

    Configure wallaby

--- wallaby.js
+++ wallaby.js
@@ -1,15 +1,23 @@
+const project = require('./aurelia_project/aurelia.json');
+
 module.exports = function(wallaby) {
+  let testSrc = [
+    { pattern: project.unitTestRunner.source, included: false },
+    'test/aurelia-karma.js'
+  ];
+
   return {
+    debug: true,
+    compilers: {
+      'src/**/*.js': wallaby.compilers.babel()
+    },
     files: [
       { pattern: 'node_modules/sinon/lib/sinon.js', instrument: false },
       { pattern: 'node_modules/bluebird/**/browser/bluebird.js', instrument: false },
       { pattern: 'src/**/*.js', load: false }
     ],
-    tests: [
-      { pattern: 'test/unit/**/*.spec.js' }
-    ],
-    env: { type: 'browser' },
-    debug: true,
+    tests: testSrc,
+    // env: { type: 'browser' },
     testFramework: 'jasmine'
   };
 };
