3a26440876703e555c8826ff18cade7633e76536
io <iolande@users.noreply.github.com>
2016-08-15 20:03:22 +0100

    Add a books route

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ export class App {
     config.title = 'Books';
     config.map([
       { route: '', name: 'home', moduleId: 'routes/index', nav: true, title: 'Home' },
-      { route: 'experiments', name: 'experiments', moduleId: 'routes/experiments', nav: true, title: 'Experiments' }
+      { route: 'experiments', name: 'experiments', moduleId: 'routes/experiments', nav: true, title: 'Experiments' },
+      { route: 'books', name: 'books', moduleId: 'routes/books', nav: true, title: 'Books' }
     ]);
     this.router = router;
   }
