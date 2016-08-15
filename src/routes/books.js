8a71841dfd9dad19efd5fcf050fa0123bd555559
io <iolande@users.noreply.github.com>
2016-08-15 22:03:47 +0100

    Add child router for books

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,3 +1,13 @@
 export class BooksRoute {
+  configureRouter(config, router) {
+    config.title = 'Books';
+    config.map([
+      { route: '', name: 'book', moduleId: '../views/book', nav: false, title: 'Books' },
+      { route: '{:bookId}', name: 'book', moduleId: '../views/book', nav: true, title: 'Book' },
+      { route: '/new', name: 'book', moduleId: '../views/book', nav: true, title: 'Add book' }
+    ]);
 
+    router.baseUrl = 'books';
+    this.router = router;
+  }
 }
