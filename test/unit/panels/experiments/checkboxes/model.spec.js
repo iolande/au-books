f5484432f1420eedd3804b8d99641993cdebb9a9
io <iolande@users.noreply.github.com>
2016-08-20 15:16:58 +0100

    Add specs to plan checkbox approaches
    The UI needs to handle several different data structures for binding to checkboxes, toggle switches and radio buttons. Start fleshing out some contrived examples

--- test/unit/panels/experiments/checkboxes/model.spec.js
+++ test/unit/panels/experiments/checkboxes/model.spec.js
@@ -30,4 +30,42 @@ describe('SocialMediaModel', () => {
     //   });
     // });
   });
+
+  describe('API contains separate properties', () => {
+    let apiModel;
+
+    beforeEach(() => {
+      apiModel = {
+        socialMedia: {
+          hasTwitter: 'YES',
+          hasFacebook: 'NO',
+          hasGithub: 'NOT_KNOWN',
+          hasBitbucket: 'NOT_KNOWN'
+        }
+      };
+    });
+
+    afterEach(() => {
+      apiModel = null;
+    });
+
+    describe('set for UI - work this out', () => {
+      it('should set an array of selected values for the UI binding...?');
+
+      it('should set hasTwitter as true');
+      it('should set hasFacebook as false');
+      it('should set hasGithub as false');
+      it('should set hasBitbucket as false');
+    });
+
+    describe('get payload for API', () => {
+      describe('when dirty cjecking is required', () => {
+        it('should return an object containing only the updated items');
+      });
+
+      describe('when dirty checking is not required', () => {
+        it('should return a single object containing all values (no dirty check)');
+      });
+    });
+  });
 });
