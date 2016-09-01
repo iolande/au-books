82704f374bc9912fa62c51e00194162246b5539d
io <iolande@users.noreply.github.com>
2016-09-01 21:46:33 +0100

    Add multi-step-form route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ export class App {
     config.title = 'Books';
     config.map([
       { route: '', name: 'home', moduleId: 'routes/index', nav: true, title: 'Home' },
+      { route: 'multi-step-form', name: 'multi-step-form', moduleId: 'routes/multi-step-form', nav: true, title: 'Multi step form' },
       { route: 'experiments', name: 'experiments', moduleId: 'routes/experiments', nav: true, title: 'Experiments' },
       { route: 'books', name: 'books', moduleId: 'routes/books', nav: true, title: 'Books' }
     ]);
